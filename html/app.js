689260eb4d7e7927a5f2f4da6ad57c5fc8fb80bd
Nicolas Urdangarin <103833940+Nadrak1@users.noreply.github.com>
2023-10-05 02:05:17 -0300

    cambios index y intento de armar carrusel

--- html/app.js
+++ html/app.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const log_in = document.querySelector(".loginButton");
+document.addEventListener("DOMContentLoaded", function () {
+  const log_in = document.querySelector(".loginButton");
 
-    log_in.onclick = function() {
-        this.innerHTML = "<div class='load'></div>";
-        setTimeout(() => {
-            this.innerHTML = "Correcto";
-            setTimeout(() => {
-                window.location.href = "index.html";
-            }, 2000);
-        }, 2000);
-    }
+  log_in.onclick = function () {
+    this.innerHTML = "<div class='load'></div>";
+    setTimeout(() => {
+      this.innerHTML = "Correcto";
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 2000);
+    }, 2000);
+  };
+  /* ------------------------------------------------
+    -------------------------------------------------------------
+    -------------------------------------------------- */
 });
